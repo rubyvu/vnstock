848d013900815c1af2c31b0611c4442d4d7f2835
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-05-20 15:27:05 +0700

    init app

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'mysql2'
+gem 'rails', '3.2.13'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem "less-rails"
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'therubyracer'
+  gem "twitter-bootstrap-rails"
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stock::Application.config.secret_token = 'cddf6152560bb8423ee9e4bce43a06d4c31081cc8a48f28bba22c64b3f1fb5ab4384fbde6d1a69ef8fbe0aced15763bbc68ff1ec585728597a659cbe77ba7cdd'
